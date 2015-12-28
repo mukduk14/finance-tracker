946fad7fcd578a2b7e43a44611d9ff2ab4866e99
Rails Udemy Course <punjabishan@gmail.com>
2015-12-28 16:03:22 +0000

    Add user and stock association

--- app/assets/javascripts/stocks.js
+++ app/assets/javascripts/stocks.js
@@ -21,4 +21,7 @@ init_stock_lookup = function() {
 
 $(document).ready(function(){
   init_stock_lookup();
+  $(".alert-success").fadeTo(2000, 500).slideUp(1000, function(){
+    $(".alert-success").alert('close');
+  });
 })
